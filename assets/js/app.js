594222db4f5899a9b5858015e5cfebc44125467e
ArtemKliuiev <117043590+ArtemKliuiev@users.noreply.github.com>
2023-09-10 18:07:47 +0300

    egit bag - close basket

--- assets/js/app.js
+++ assets/js/app.js
@@ -89,6 +89,17 @@ function filtration(number){
 function basket(){
     body.classList.toggle('basket-active');
 }
+document.addEventListener('click', function(event) {
+    const basket = document.querySelector('.basket')
+    const basketIcon = document.querySelector('.header__basket')
+    if (basket.contains(event.target)) {
+
+    }else if(basketIcon.contains(event.target)){
+
+    }else{
+        body.classList.remove('basket-active');
+    }
+});
 
 //Адаптивное меню провиль
 function filtrationProfile(number){
